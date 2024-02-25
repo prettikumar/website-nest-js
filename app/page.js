85310a27b8ad3dfa37f123a40aa0dc10c265a865
swaraj Saxena <swarishraj00@gmail.com>
2024-02-25 15:32:57 +0530

    padding corrected

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className='flex flex-col items-center justify-center min-h-screen gap-4 p-24'>
+    <main className='flex flex-col items-center justify-center min-h-screen gap-4 p-4'>
       <div className='font-medium'>Afta Business</div>
       <div className='max-w-lg text-3xl font-bold text-center'>
         Exciting Developments Underway!
